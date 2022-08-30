a31f4b87427dd37b85da83c9c4ba8abd7dc89557
BGC <46126246+baegichan@users.noreply.github.com>
2022-08-30 12:09:31 +0900

    Update AutoMount.sh

--- AutoMount/AutoMount.sh
+++ AutoMount/AutoMount.sh
@@ -18,18 +18,17 @@ fdisk $i <<END
                         w
                         q
 END
-                        mkdir /root$(echo "$i" | cut -c 5-)
-mkfs.ext4 "$i""1" << EOF
+                        mkdir /root$(echo "$i" | cut -c 5-) >/dev/null 2>&1
+                        mkfs.ext4 "$i""1" << EOF >/dev/null 2>&1
                         y
 EOF
 DIRNAME=/root$(echo "$i" | cut -c 5-)
-                        if      [ $(cat /etc/fstab | grep $i ) -z ] ; then
+                        if  [$(cat /etc/fstab | grep $i ) == ""]>/dev/null 2>&1 ; then
                                 mount "$i""1" $DIRNAME
                                 echo "$i""1   $DIRNAME   ext4 defaults 0 0" >> /etc/fstab
                         fi
+        echo "[ OK ] $i done"
                 done ;;
-
         *) echo "No parameter" ;;
 
 esac
-
